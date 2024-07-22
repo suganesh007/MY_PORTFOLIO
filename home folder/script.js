e684698072e095a3f2d24aec84a50f1260bed5e6
Suganesh <sgeethasugan2003@gmail.com>
2024-07-22 09:37:47 +0530

    commited all index portfolio and other files

---
+++ home folder/script.js
@@ -0,0 +1,13 @@
+var div = document.getElementById("header")
+var section1 = document.getElementById("section1")
+
+function swipeUp()
+{
+    
+    
+    div.style.height = "0"
+    section1.style.height = "100vh"
+
+
+}
+
